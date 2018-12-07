e0d60e8c036e8c74cb33142325daf738a2ec999a
abdessamad <abdou4101@gmail.com>
2018-12-08 00:20:00 +0100

    modif Part

--- src/main/java/Part.java
+++ src/main/java/Part.java
@@ -41,58 +41,11 @@ public class Part {
 	}
 	
 	public static void main(String[] args) {
-//		List<Integer> list = new ArrayList<Integer>();
-//		list.add(new Integer(1));
-//		list.add(2);
-//		list.add(3);
-//		list.add(6);
-//		list.add(5);
-//		
-//		 List<Item> list = Arrays.asList(new Item("apple", 10, new BigDecimal("9.99")),
-//	                new Item("banana", 20, new BigDecimal("19.99")),
-//	                new Item("orang", 10, new BigDecimal("29.99")),
-//	                new Item("watermelon", 10, new BigDecimal("29.99")),
-//	                new Item("papaya", 20, new BigDecimal("9.99")),
-//	                new Item("apple", 10, new BigDecimal("9.99")),
-//	                new Item("banana", 10, new BigDecimal("19.99")),
-//	                new Item("apple", 20, new BigDecimal("9.99"))
-//	        );
-//		final List<Object> list = Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","X");
 
-	
-//		Collection<List<Integer>> liste = Part.part(list, 2);
-//		System.out.println(liste);
-//		
-//		System.out.println(liste.stream()
-//        .flatMap(List::stream)
-//        .collect(Collectors.toList()));
-		
-//		final List<String> list = Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","X");
-//
-//        final AtomicInteger counter = new AtomicInteger(0);
-//        final int size = 5;
-//
-//        final Collection<List<String>> partitioned = list.stream()
-//                .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / size))
-//                .values();
-
-//        partitioned.forEach(System.out::println);
 		
 		List<String> liste = Arrays.asList("A","B","C","D","E","F","G","H","I","G","K","L","G","N","O","P","X");
 		Map<String, Integer> counts = new HashMap<String, Integer>();
-		
-//		for(String car : liste)
-//	    {
-//			if(counts.containsKey(car)) {
-//				counts.put(car, counts.get(car) + 1);
-//			} else {
-//				counts.put(car, 1);
-//			}
-//	    }
-		
-//		liste.stream().filter(n -> Collections.frequency(liste, n) > 1)
-//		.forEach(System.out::print);
-		
+
 		System.out.println(liste);// pas d'effet de bord
 		
 		List<String> liste1 = new ArrayList<String>(liste);
